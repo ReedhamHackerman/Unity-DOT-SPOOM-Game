a788188a935c40b6f6b4b10e11a24f5cbe838c83
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-04-29 08:31:39 -0400

    Collision Event and Damage
    Do Damage Based on Collision Event

---
+++ Assets/Scripts/Systems/CollisionSystem.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+public class CollisionSystem : SystemBase
+{
+    private struct CollisionSystemJob : ICollisionEventsJob
+    {
+        public BufferFromEntity<CollisionBuffer> collisions;
+        public void Execute(CollisionEvent collisionEvent)
+        {
+            if (collisions.Exists(collisionEvent.EntityA))
+            {
+                collisions[collisionEvent.EntityA].Add(new CollisionBuffer() { entity = collisionEvent.EntityB });
+            }
+            if (collisions.Exists(collisionEvent.EntityB))
+            {
+                collisions[collisionEvent.EntityB].Add(new CollisionBuffer() { entity = collisionEvent.EntityA });
+            }
+        }
+    }
+
+    //private struct TriggerSystemJob : ITriggerEventsJob
+    //{
+    //    public void Execute(TriggerEvent triggerEvent)
+    //    {
+           
+    //    }
+    //}
+
+
+    protected override void OnUpdate()
+    {
+         var pw = World.GetOrCreateSystem<BuildPhysicsWorld>().PhysicsWorld;
+         var sim = World.GetOrCreateSystem<StepPhysicsWorld>().Simulation;
+
+        Entities.ForEach((DynamicBuffer<CollisionBuffer> collisions) => {
+
+            collisions.Clear();
+        }
+        ).Run();
+
+         var colJobHandle =  new CollisionSystemJob()
+         { 
+             collisions = GetBufferFromEntity<CollisionBuffer>()
+         }.Schedule(sim,ref pw,this.Dependency);
+         colJobHandle.Complete();
+
+    }
+   
+}
